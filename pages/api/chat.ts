7d993ea17e1358ac95fc54bbba71d54bf74a83b8
Toby Brown <hi@tobyb.dev>
2023-07-30 17:48:28 -0400

    working!

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,5 +1,6 @@
 import { Configuration, OpenAIApi } from "openai-edge";
 import { OpenAIStream, StreamingTextResponse } from "ai";
+import { kv } from "@vercel/kv";
 
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,14 +9,27 @@ const openai = new OpenAIApi(config);
 
 export const runtime = "edge";
 
-export async function POST(req: Request) {
+export default async function POST(req: Request) {
   const { messages } = await req.json();
+
+  const prompt = `You are a thoughtful AI chatbot that asks very deep, and existential-causing questions inside a messaging interface. You will start the conversation, and the person you are talking to will reply with an answer. Keep these questions short and minimal (straight to the point). Preferably only one sentence. Here is an example: how does it feel to know that your existent is simply insignificant in this constant ever-lasting universe? Make sure that your question is different every time. Here are your past responses: . Try to continue on the conversation.`;
+
+  const message = `${prompt}\n\n${messages}`;
 
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     stream: true,
-    messages,
+    temperature: 1.5,
+    messages: [{ role: "user", content: message }],
   });
-  const stream = OpenAIStream(response);
+
+  const stream = OpenAIStream(response, {
+    async onCompletion(completion: string) {
+      await kv.hset("responses", {
+        response: completion,
+        createdAt: new Date().toISOString(),
+      });
+    },
+  });
   return new StreamingTextResponse(stream);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,24 +8,24 @@ export default function Home() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
     <div
-      className={`flex w-max mx-auto min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex mx-auto min-h-screen flex-col items-center justify-between p-24 w-screen ${inter.className}`}
     >
-      <div className="w-full max-w-2xl">
-        <H1>You&apos;re ChatGPT</H1>
+      <div className="w-full bg-red-400">
+        <H1 className="mb-8">You&apos;re ChatGPT</H1>
         <UserBubble>Hey! I&apos;m ChatGPT, your virtual assistant!</UserBubble>
         {messages.map((m) => (
           <div key={m.id}>
             {m.role === "user" ? (
               <UserBubble>{m.content}</UserBubble>
             ) : (
-              <AIBubble>
-                {m.content}
-              </AIBubble>
+              <AIBubble>{m.content}</AIBubble>
             )}
-            {m.content}
           </div>
         ))}
-        <form className="space-x-2 fixed bottom-8 max-w-2xl" onSubmit={handleSubmit}>
+        <form
+          className="space-x-2 fixed bottom-8 w-full bg-green-400"
+          onSubmit={handleSubmit}
+        >
           <input
             className="rounded-xl p-2 outline-none bg-gray-100"
             placeholder="Your deep reply..."
@@ -46,7 +46,7 @@ export default function Home() {
 
 function UserBubble({ children }: any) {
   return (
-    <div className="bg-[#007AFF] p-2 rounded-xl w-max">
+    <div className="bg-[#007AFF] p-2 rounded-xl w-max my-4">
       <P className="text-white">{children}</P>
     </div>
   );
