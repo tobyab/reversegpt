4e8a475c2187c55883154d82b8e90ccbe4544212
Toby Brown <hi@tobyb.dev>
2023-07-30 11:09:32 -0400

    may or may not be finished

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,38 +1,21 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-import { Configuration, OpenAIApi } from 'openai-edge'
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
 
-export const runtime = 'edge'
-const openai = new OpenAIApi(configuration)
+export const runtime = "edge";
 
-type Data = {
-  name: string
-}
+export async function POST(req: Request) {
+  const { messages } = await req.json();
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  if (req.method === "POST") {
-    try {
-      const resp = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: req.body.message,
-        temperature: 0.7,
-        stream: true,
-      })
-      
-      const data = await resp.json();
-
-      return res.status(200).json({ data } as any) // TODO: change later
-    } catch (error: any | undefined) {
-      console.log(error.message)
-      return res.status(500).json(error.message)
-    }
-  }
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    stream: true,
+    messages,
+  });
+  const stream = OpenAIStream(response);
+  return new StreamingTextResponse(stream);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,46 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { H1, P } from "../components/typography"
-import { useState } from 'react'
+import { Inter } from "next/font/google";
+import { H1, P } from "@/components/typography";
+import { useChat } from "ai/react";
 
-const inter = Inter({ subsets: ['latin'] })
-export const runtime = 'edge'
-
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [prompt, setPrompt] = useState(`You are a thoughtful AI chatbot that asks very deep, and existential-causing questions inside a messaging interface. You will start the conversation, and the person you are talking to will reply with an answer.
-
-  Keep these questions short and minimal (straight to the point). Preferably only one sentence. Can you follow the format, or do something similar to the following examples:"`)
-
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
     <div
       className={`flex w-max mx-auto min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="w-full max-w-2xl">
-        <H1>Your convo w/ DeepGPT</H1>
-        <div className="space-y-4 mt-8 place-items-between">
-          <UserBubble>Hello! I&apos;m ChatGPT, your virtual assistant!</UserBubble>
-          <AIBubble>Super duper deep question here</AIBubble>
+        <H1>You&apos;re ChatGPT</H1>
+        <UserBubble>Hey! I&apos;m ChatGPT, your virtual assistant!</UserBubble>
+        {messages.map((m) => (
+          <div key={m.id}>
+            {m.role === "user" ? (
+              <UserBubble>{m.content}</UserBubble>
+            ) : (
+              <AIBubble>
+                {m.content}
+              </AIBubble>
+            )}
+            {m.content}
           </div>
-          <form className="space-x-2 fixed bottom-8 max-w-2xl">
-            <input
-              className="rounded-xl p-2 outline-none bg-gray-100"
-              placeholder="Your deep reply..."
-            />
-            <button type="submit" className="rounded-xl p-2 bg-gray-100 hover:bg-gray-200">Send</button>
-          </form>
-        </div>
+        ))}
+        <form className="space-x-2 fixed bottom-8 max-w-2xl" onSubmit={handleSubmit}>
+          <input
+            className="rounded-xl p-2 outline-none bg-gray-100"
+            placeholder="Your deep reply..."
+            value={input}
+            onChange={handleInputChange}
+          />
+          <button
+            type="submit"
+            className="rounded-xl p-2 bg-gray-100 hover:bg-gray-200"
+          >
+            Send
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
-
-function Option({ setActive, option }: any) {
-  return (
-    <P className="capitalize">
-      <a
-        className="py-2 cursor-pointer"
-        onClick={() => setActive(option)}
-      >
-        {option}
-      </a>
-    </P>
   );
 }
 
@@ -52,7 +49,7 @@ function UserBubble({ children }: any) {
     <div className="bg-[#007AFF] p-2 rounded-xl w-max">
       <P className="text-white">{children}</P>
     </div>
-  )
+  );
 }
 
 function AIBubble({ children }: any) {
@@ -60,5 +57,5 @@ function AIBubble({ children }: any) {
     <div className="bg-gray-100 p-2 rounded-xl w-max">
       <P>{children}</P>
     </div>
-  )
+  );
 }
